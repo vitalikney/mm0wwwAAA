1fafe52e1aaae5926ac73f9cf4a217ef15183cc1
vitalikney <57719362+vitalikney@users.noreply.github.com>
2020-03-02 19:20:44 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+playerFire = False
+
+# Groups
+mouse_sprite = pygame.sprite.Group()
+whiteStormTroopers_sprite = pygame.sprite.Group()
+firstPlay_sprite = pygame.sprite.Group()
+player_sprite = pygame.sprite.Group()
+backGround_sprite = pygame.sprite.Group()
+tie_fighter_sprite = pygame.sprite.Group()
+hp = 20
